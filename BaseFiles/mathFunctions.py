fcee46b499a3cb86c36e4055312ec8f64d82c1bd
ramnreddy15 <ram.n.reddy15@gmail.com>
2021-03-21 19:26:56 -0700

    Jyputer notebook

--- BaseFiles/mathFunctions.py
+++ BaseFiles/mathFunctions.py
@@ -3,23 +3,32 @@ import math
 """Q1 A"""
 """This function return the mean, standard deviation, number of elements in list/vector""" 
 def calculateStatistics(listOne, type):
-    populationMean = 0.0
+    mean = 0.0
     standardDeviation = 0.0
     size = len(listOne)
     
     #Calculates sample mean of list
-    populationMean = sum(listOne)/size
+    mean = sum(listOne)/size
 
     #Calculates sample standard deviation of list
     for i in range(0, size): #First calculates variance
-        standardDeviation += pow((listOne[i] - populationMean), 2)
-    if(type == "sample")
+        standardDeviation += pow((listOne[i] - mean), 2)
+    if(type == "sample"):
         standardDeviation = standardDeviation/(size-1)
     else:
         standardDeviation = standardDeviation/(size)
     standardDeviation = math.sqrt(standardDeviation)
 
-    return populationMean, standardDeviation, size
+    return mean, standardDeviation, size
+
+# theList = [5.99342831, 4.7234714 , 6.29537708, 8.04605971, 4.53169325,
+# 4.53172609, 8.15842563, 6.53486946, 4.06105123, 6.08512009]
+
+# mean, standardDeviation, size = calculateStatistics(theList, "population")
+
+# print(mean)
+# print(standardDeviation)
+# print(size)
 
 """Q1 B"""
 """This method return true if the means have a statistically significant differnece"""
@@ -45,16 +54,11 @@ def calculateZStat(sampleList, populationList):
     else:
         print("This gene is a good gene with smaller than the change of " + str(p) + " of expressing with a Z statistic of " + str(zStat))
 
-# sampleList = [0, 4.7234714 , 6.29537708, 8.04605971, 4.53169325, 
-# 4.53172609, 8.15842563, 6.53486946, 4.06105123, 6.08512009]
+
+sampleList = [5.99342831, 4.7234714 , 6.29537708, 8.04605971, 4.53169325,
+4.53172609, 8.15842563, 6.53486946, 4.06105123, 6.08512009]
     
-# populationList = [0, 4.7234714 , 6.29537708, 8.04605971, 4.53169325, 
-# 4.53172609, 8.15842563, 6.53486946, 4.06105123, 6.08512009]
+populationList = [5.99342831, 4.7234714 , 6.29537708, 8.04605971, 4.53169325,
+4.53172609, 8.15842563, 6.53486946, 4.06105123, 6.08512009]
 
-# populationMean, populationStandardDeviation, populationSize = calculateStatistics(populationList, "population")
-
-# print(populationMean)
-# print(populationStandardDeviation)
-# print(populationSize)
-
-# calculateZStat(sampleList, populationList)
+calculateZStat(sampleList, populationList)
